ca1dd42bac0e7e60072acdf782b40ae016c0d790
tianYu <tianyu@gouwangminhaodeMacBook-Pro.local>
2018-10-17 09:33:22 +0800

    插入XML成功

---
+++ venv/XMLtest.py
@@ -0,0 +1,30 @@
+import xml.dom.minidom
+
+doc=xml.dom.minidom.Document()
+root=doc.createElement('News')
+doc.appendChild(root)
+
+managerList = [{'name' : 'joy',  'age' : 27, 'sex' : '女'},
+               {'name' : 'tom', 'age' : 30, 'sex' : '男'},
+               {'name' : 'ruby', 'age' : 29, 'sex' : '女'}
+]
+
+for i in managerList:
+    nodeTopic=doc.createElement('Topic')
+    nodeTopic.appendChild(doc.createTextNode('sports'))
+    nodeLink=doc.createElement('Link')
+    nodeLink.appendChild(doc.createTextNode('baidu.com'))
+    nodeTime=doc.createElement('Time')
+    nodeTime.appendChild(doc.createTextNode('10-17'))
+    nodeTitle=doc.createElement('Title')
+    nodeTitle.appendChild(doc.createTextNode(str(i['name'])))
+    nodeText=doc.createElement('Text')
+    nodeText.appendChild(doc.createElement(str(i['age'])))
+    root.appendChild(nodeTopic)
+    root.appendChild(nodeLink)
+    root.appendChild(nodeTime)
+    root.appendChild(nodeTitle)
+    root.appendChild(nodeText)
+fp=open('/Users/tianyu/news.xml','w')
+doc.writexml(fp, indent='', addindent='\t', newl='\n', encoding="utf-8")
+
--- venv/scrap.py
+++ venv/scrap.py
@@ -4,11 +4,24 @@ from urllib.request import urlopen
 import re
 
 
-url='http://mil.news.sina.com.cn/roll/index.d.html?cid=57918'
-html = urlopen(url).read().decode('utf-8')
-res=re.findall(r'<a href="(.*?)" target="_blank">(.+?)</a>',html)
+url=['http://mil.news.sina.com.cn/roll/index.d.html?cid=57918',]
+count=0#用于统计总共爬取新闻数量
+html = urlopen(url[0]).read().decode('utf-8')
+res=re.findall(r'<a href="(.*?)" target="_blank">(.+?)</a>',html)#用于爬取超链接和新闻标题
+
 for i in res:
-    print(i)
+    try:
+        urli=i[0]
+        htmli=urlopen(urli).read().decode('utf-8')
+        time=re.findall(r'<span class="date">(.*?)</span>',htmli)
+        resp=re.findall(r'<p>(.*?)</p>',htmli)
+        print(i)
+        print(time)
+        print(resp)
+        count+=1
+    except:
+        print(count)
+        break
 #print(html)
 #bsObj = BeautifulSoup(spData, 'html.parser')
 #bsObj.encode("GBK")
